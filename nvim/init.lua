1abf9f7d86ac7e5063bf7f7260cf165ee0b2a4dd
Vaibhav Mhaske <vaibhavmhaske.here@gmail.com>
2024-10-12 00:15:21 +0530

    new nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,5 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 if vim.g.vscode then
-    require "user.vscode_keymaps"
+    require "vs-code.vscode_keymaps"
 else
     require "config.lazy"
 end
-
